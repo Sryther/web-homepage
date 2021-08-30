e1011a62e6858643bb29feff2ed11e7c12d0cc8d
Paul Rey <paul@iterop.com>
2021-08-31 00:54:20 +0200

    Fix

--- dist/index.js
+++ dist/index.js
@@ -24,43 +24,40 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/assets/index.html'));
 });
 app.get('/random', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        let ip = req.ip;
-        if (req.ip !== "127.0.0.1" && req.ip === "::1") {
-            ip = "88.171.49.146"; // For test purposes
-        }
-        const geoloc = goip.lookup(ip);
+    let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    console.log(ip);
+    if (req.ip !== "127.0.0.1" && req.ip === "::1") {
+        ip = "88.171.49.146"; // For test purposes
+    }
+    const geoloc = goip.lookup(ip);
+    const pexelsQuery = {
+        page: Math.floor(1000 * Math.random()),
+        per_page: 1,
+        orientation: 'landscape',
+        locale: 'en-US',
+        query: 'nature'
+    };
+    if (geoloc !== null && geoloc !== undefined && geoloc.ll !== null && geoloc.ll !== undefined) {
         const weather = yield new AsyncWeather();
         weather.setApiKey(process.env.OPENWEATHER_API_KEY);
         weather.setUnits('metric');
         weather.setCoordinates(geoloc.ll[1], geoloc.ll[0]);
         weather.setLang(geoloc.country.toLowerCase());
         const resultWeather = yield weather.getAllWeather();
-        const pexelsQuery = {
-            page: Math.floor(1000 * Math.random()),
-            per_page: 1,
-            orientation: 'landscape',
-            locale: 'en-US',
-            query: 'nature'
-        };
         if (resultWeather.weather[0].icon.indexOf("n") !== -1) {
             pexelsQuery.query = "galaxy";
             pexelsQuery.page = Math.floor(100 * Math.random());
         }
-        const response = yield pexelsApp.photos.search(pexelsQuery);
-        let photo;
-        if (response["photos"][0].src.large2x) {
-            photo = response["photos"][0].src.large2x;
-        }
-        else {
-            photo = response["photos"][0].src.large;
-        }
-        res.send(photo);
     }
-    catch (e) {
-        console.log(e);
-        res.status(500).send(e.stack);
+    const response = yield pexelsApp.photos.search(pexelsQuery);
+    let photo;
+    if (response["photos"][0].src.large2x) {
+        photo = response["photos"][0].src.large2x;
     }
+    else {
+        photo = response["photos"][0].src.large;
+    }
+    res.send(photo);
 }));
 app.use('/assets', express.static('assets'));
 app.listen(port, () => {
